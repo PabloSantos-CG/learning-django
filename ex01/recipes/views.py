920effb57053911414cdaaf8150d2a8f8fef2e8f
PabloSantos-CG <comercial-pablo@outlook.com>
2025-02-12 15:14:39 -0300

    add route and template

--- ex01/recipes/views.py
+++ ex01/recipes/views.py
@@ -4,3 +4,5 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'recipes/pages/home.html')
 
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe.html')
