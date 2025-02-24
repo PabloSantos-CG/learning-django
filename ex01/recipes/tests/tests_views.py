4131d3057ee628ed808fd33d294941539caa222e
PabloSantos-CG <comercial-pablo@outlook.com>
2025-02-24 12:10:26 -0300

    separates the base configuration tests

--- ex01/recipes/tests/tests_views.py
+++ ex01/recipes/tests/tests_views.py
@@ -1,39 +1,40 @@
-from django.test import TestCase
+# from django.test import TestCase
 from django.urls import resolve, reverse
 from recipes import views
-from recipes.models import Category, Recipe, User
+from .test_recipe_base import RecipeTestBase
+# from recipes.models import Category, Recipe, User
 
 
-class RecipeTestBase(TestCase):
-    def setUp(self) -> None:
-        category = Category.objects.create(name='Category')
-        author = User.objects.create_user(
-            first_name='user',
-            last_name='name',
-            username='username',
-            password='123456',
-            email='username@email.com',
-        )
-        recipe = Recipe.objects.create(
-            category=category,
-            author=author,
-            title='Recipe Title',
-            description='Recipe Description',
-            slug='recipe-slug',
-            preparation_time=10,
-            preparation_time_unit='Minutos',
-            servings=5,
-            servings_unit='Porções',
-            preparation_steps='Recipe Preparation Steps',
-            preparation_steps_is_html=False,
-            is_published=True,
-        )
-        return super().setUp()
+# class RecipeTestBase(TestCase):
+#     def setUp(self) -> None:
+#         category = Category.objects.create(name='Category')
+#         author = User.objects.create_user(
+#             first_name='user',
+#             last_name='name',
+#             username='username',
+#             password='123456',
+#             email='username@email.com',
+#         )
+#         recipe = Recipe.objects.create(
+#             category=category,
+#             author=author,
+#             title='Recipe Title',
+#             description='Recipe Description',
+#             slug='recipe-slug',
+#             preparation_time=10,
+#             preparation_time_unit='Minutos',
+#             servings=5,
+#             servings_unit='Porções',
+#             preparation_steps='Recipe Preparation Steps',
+#             preparation_steps_is_html=False,
+#             is_published=True,
+#         )
+#         return super().setUp()
 
 
 class RecipeViewsTest(RecipeTestBase):
-    def tearDown(self) -> None:
-        return super().tearDown()
+    # def tearDown(self) -> None:
+    #     return super().tearDown()
 
     def test_recipes_view_home(self):
         view = resolve(reverse('recipes:home'))
@@ -50,15 +51,18 @@ class RecipeViewsTest(RecipeTestBase):
     def test_recipes_home_loads_template_correct(self):
         response = self.client.get(reverse('recipes:home'))
         self.assertTemplateUsed(response, 'recipes/pages/home.html')
-        
 
-        # content = response.content.decode('utf-8')
-        # response_context_recipes = response.context['recipes']
+    def test_recipe_home_template_loads_recipes(self):
+        self.make_recipe()
 
-        # self.assertIn('Recipe Title', content)
-        # self.assertIn('10 Minutos', content)
-        # self.assertIn('5 Porções', content)
-        # self.assertEqual(len(response_context_recipes), 1)
+        response = self.client.get(reverse('recipes:home'))
+        content = response.content.decode('utf-8')
+        response_context_recipes = response.context['recipes']
+
+        self.assertIn('Recipe Title', content)
+        self.assertIn('10 Minutos', content)
+        self.assertIn('5 Porções', content)
+        self.assertEqual(len(response_context_recipes), 1)
 
     def test_recipe_details_404(self):
         recipe_details = reverse('recipes:details', kwargs={'id': 9999})
