c8120004778acfa01728cbaa46b8e13393c714c2
PabloSantos-CG <comercial-pablo@outlook.com>
2025-02-28 17:30:54 -0300

    update

--- ex01/authors/views.py
+++ ex01/authors/views.py
@@ -4,6 +4,9 @@ from .forms import RegisterForm
 
 
 def register(request: HttpRequest):
-    form = RegisterForm()
+    if request.POST:
+        form = RegisterForm(request.POST)
+    else:
+        form = RegisterForm()
 
     return render(request, 'authors/pages/home.html', {'form': form})
