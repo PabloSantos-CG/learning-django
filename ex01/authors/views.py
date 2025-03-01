05fa5fd66b72b312da205a6e47cdc8390b2b9ad7
PabloSantos-CG <comercial-pablo@outlook.com>
2025-03-01 11:32:44 -0300

    add session

--- ex01/authors/views.py
+++ ex01/authors/views.py
@@ -1,12 +1,19 @@
-from django.http import HttpRequest
-from django.shortcuts import render
+from django.http import Http404, HttpRequest
+from django.shortcuts import redirect, render
 from .forms import RegisterForm
 
 
 def register(request: HttpRequest):
-    if request.POST:
-        form = RegisterForm(request.POST)
-    else:
-        form = RegisterForm()
+    form = RegisterForm()
 
     return render(request, 'authors/pages/home.html', {'form': form})
+
+def create(request: HttpRequest):
+    if not request.POST:
+        raise Http404()
+
+    POST = request.POST
+    request.session['register_form_data'] = POST
+    form = RegisterForm(POST)
+    
+    return redirect('authors:register')
